00cfd36b7a07891ab6c27c515aba4f0a2a8f62cf
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-05-07 18:02:07 +0200

    improve tweets view

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('minitwrApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'monospaced.elastic'
 ])
   .config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
