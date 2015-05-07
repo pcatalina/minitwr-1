dd09f96ddc1560db1f7fb122321ccfae22e67f4b
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-05-07 23:43:58 +0200

    make button on main page to redirect depending on auth status

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('minitwrApp')
-  .controller('MainCtrl', function($scope) {
+  .controller('MainCtrl', function($scope, $location, Auth) {
 
+    $scope.enter = function() {
+      if(Auth.isLoggedIn())
+        $location.path( "/tweets" );
+      else
+        $location.path( "/login" );
+    }
   });
