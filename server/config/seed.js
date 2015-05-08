836002d269e15ff9173924afc341991ab305fcc9
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-05-08 12:28:36 +0200

    add explicit fixed '_id' fields for user's db seeds

--- server/config/seed.js
+++ server/config/seed.js
@@ -6,14 +6,21 @@
 'use strict';
 
 var User = require('../api/user/user.model');
+var mongoose = require('mongoose');
+
+// add '_id' field explicitly,
+// so seed users would have same ids after server restart
+UserSchema.add({ _id: mongoose.Types.ObjectId });
 
 User.find({}).remove(function() {
   User.create({
+      _id: mongoose.Types.ObjectId('00000000test'),
       provider: 'local',
       name: 'Test User',
       email: 'test@test.com',
       password: 'test'
     }, {
+      _id: mongoose.Types.ObjectId('0000000admin'),
       provider: 'local',
       role: 'admin',
       name: 'Admin',
