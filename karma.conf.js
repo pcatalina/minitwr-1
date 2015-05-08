3c36cd7ab1e7a64529347f74217efc0d83bb97d8
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-05-08 22:50:45 +0200

    karma: fix missing angular-moment

--- karma.conf.js
+++ karma.conf.js
@@ -24,6 +24,7 @@ module.exports = function(config) {
       'client/bower_components/angular-socket-io/socket.js',
       'client/bower_components/angular-ui-router/release/angular-ui-router.js',
       'client/bower_components/angular-elastic/elastic.js',
+      'client/bower_components/angular-moment/angular-moment.js',
       'client/app/app.js',
       'client/app/app.coffee',
       'client/app/**/*.js',
