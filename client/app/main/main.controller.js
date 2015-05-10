7fe0abfbcdbc3cba1df6c18b53aebcf63c24d348
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-05-10 19:47:47 +0200

    autoredirect to tweets from main if authenticated

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,6 +3,9 @@
 angular.module('minitwrApp')
   .controller('MainCtrl', function($scope, $location, Auth) {
 
+    if(Auth.isLoggedIn())
+      $location.path( "/tweets" );
+
     $scope.enter = function() {
       if(Auth.isLoggedIn())
         $location.path( "/tweets" );
